7e2b3e95daa6f6327cb68dc958166046a6636875
arora-72 <indresh.arora2016@vitstudent.ac.in>
2018-03-11 01:02:29 +0530

    tableview updation problem

---
+++ arcs/ColorExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ColorExtension.swift
+//  arcs
+//
+//  Created by Indresh Arora on 10/03/18.
+//  Copyright © 2018 Indresh Arora. All rights reserved.
+//
+
+import Foundation
---
+++ arcs/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  arcs
+//
+//  Created by Indresh Arora on 10/03/18.
+//  Copyright © 2018 Indresh Arora. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
